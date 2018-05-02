06e55a78b726d499bb45371f6be995f5a9cc298e
Riiuku <lukasz0596@gmail.com>
2018-05-02 12:43:22 +0200

    Initial commit

---
+++ src/main/java/pl/lukasz/discussionforum/DiscussionforumApplication.java
@@ -0,0 +1,12 @@
+package pl.lukasz.discussionforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussionforumApplication.class, args);
+	}
+}
