09d319b4bf551d4369535558cb0ccf07eed04e77
Riiuku <lukasz0596@gmail.com>
2018-05-04 22:33:03 +0200

    passwordEncoder

--- src/main/java/pl/lukasz/discussionforum/config/security/PasswordEncoderConfig.java
+++ src/main/java/pl/lukasz/discussionforum/config/security/PasswordEncoderConfig.java
@@ -4,8 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
-public class passwordEncoderConfig {
+public class PasswordEncoderConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
